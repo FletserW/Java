f432767800a3672ca7a04be96d7db0433052a0fa
FletserW <130801024+FletserW@users.noreply.github.com>
2023-06-09 11:49:38 -0300

    Base salarial

---
+++ Aula_26.05/BaseSalarial/Administrador.java
@@ -0,0 +1,29 @@
+package BaseSalarial;
+
+public class Administrador extends Empregado {
+    private double ajudaDeCusto;
+
+    public Administrador() {
+        // Construtor padrão
+    }
+
+    public Administrador(String nome, String endereco, String telefone, int codigoSetor, double salarioBase, double imposto, double ajudaDeCusto) {
+        super(nome, endereco, telefone, codigoSetor, salarioBase, imposto);
+        this.ajudaDeCusto = ajudaDeCusto;
+    }
+
+    // Método getter e setter para ajudaDeCusto
+    public double getAjudaDeCusto() {
+        return ajudaDeCusto;
+    }
+
+    public void setAjudaDeCusto(double ajudaDeCusto) {
+        this.ajudaDeCusto = ajudaDeCusto;
+    }
+
+    @Override
+    public double calcularSalario() {
+        return super.calcularSalario() + ajudaDeCusto;
+    }
+}
+
