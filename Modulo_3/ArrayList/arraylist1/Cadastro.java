da2edccc992df09bcf76ff1ab2ec3ca4587b710b
FletserW <reidosgames147@hotmail.com>
2023-09-14 18:45:03 -0300

    arraylist1.0

---
+++ Modulo_3/ArrayList/arraylist1/Cadastro.java
@@ -0,0 +1,12 @@
+package arraylist1;
+
+import java.util.ArrayList; 
+Class Cadastro{
+    public static void main(String[] args) {
+        ArrayList <Pessoa> list = new ArrayList <Pessoa>();
+        Pessoa P = new Pessoa();
+        P.setNome("Nicolas Miguel");
+        P.setCpf("20055217592");
+        P.setNasc
+    }
+}
---
+++ Modulo_3/ArrayList/arraylist1/Pessoa.java
@@ -0,0 +1,27 @@
+package arraylist1;
+
+class Pessoa{
+    private String nome;
+    private String cpf;
+    private String nasc;
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public String getNome(){
+        return.nome;
+    }
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+    public String getCpf(){
+        return.cpf;
+    }
+    public void setNasc(String nasc){
+        this.nasc = nasc;
+    }
+    public String getNasc(){
+        return.nasc;
+    }
+    
+}
